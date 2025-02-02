aab4c1a310c3f7a334c13fee816831b6a062e2a1
prachidimote <81797753+prachidimote@users.noreply.github.com>
2025-02-02 22:33:54 +0530

    added size limit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 
 //routes
